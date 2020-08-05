fcb311e8ebae56628697771407067c34c76c26cc
imankovskaya <irina.mankovskaya@noveogroup.com>
2020-08-05 14:53:46 +0700

    Fix font names, add toolbar

--- app/src/main/java/com/mankovskaya/mviexample/MainActivity.kt
+++ app/src/main/java/com/mankovskaya/mviexample/MainActivity.kt
@@ -2,12 +2,19 @@ package com.mankovskaya.mviexample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.view_toolbar.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 
 }
